9a750aed9931041e226e5764fd8d52a889bfd634
SmellyGhost <60049391+Smelly-Ghost@users.noreply.github.com>
2026-08-08 13:33:02 -0700

    Implement toggleable Auto-Run on B button press

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -133,6 +133,13 @@ void FieldGetPlayerInput(struct FieldInput *input, u16 newKeys, u16 heldKeys)
         input->dpadDirection = DIR_WEST;
     else if (heldKeys & DPAD_RIGHT)
         input->dpadDirection = DIR_EAST;
+
+        // Toggle Auto-Run when B is pressed on foot
+    if ((newKeys & B_BUTTON) && (!ArePlayerFieldControlsLocked())
+    && (gPlayerAvatar.flags & (PLAYER_AVATAR_FLAG_DASH | PLAYER_AVATAR_FLAG_ON_FOOT)))
+    {
+        gRunToggleBtnSet = TRUE;
+    }
 }
 
 int ProcessPlayerFieldInput(struct FieldInput *input)
